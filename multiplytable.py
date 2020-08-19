0aaade937feb841d4913d628da07452041461a91
Alison Manning <alisonlaurenmanning@gmail.com>
2020-08-19 14:39:08 -0500

    adding multiplytable.py

---
+++ multiplytable.py
@@ -0,0 +1,7 @@
+print("Welcome to your Multiplication Table: ")
+for i in range(1, 5):
+    for j in range(1, 5):
+        print('{0}  *  {1}  =  {2}'.format(i, j, i*j))
+    print('........')
+    
+
