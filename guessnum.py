0a594853511a78299838c2ca026acaa18d54acc4
Alison Manning <alisonlaurenmanning@gmail.com>
2020-08-19 15:14:57 -0500

    adding guessnum.py

---
+++ guessnum.py
@@ -0,0 +1,13 @@
+
+num_guess = ' '
+print("I'm thinking of a number between 1 and 10, what is it?: ")
+
+
+while num_guess != "no":
+    num_guess = int(input("Would you like to try again??: "))
+    if not num_guess != 33:
+        print("You got it!")
+        break
+    
+
+        
