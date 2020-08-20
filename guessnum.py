3362fa78a533476c153a8f47befeaf2d79eda06a
Alison Manning <alisonlaurenmanning@gmail.com>
2020-08-20 16:28:13 -0500

    Update guessnum.py
    added a try/except

--- guessnum.py
+++ guessnum.py
@@ -1,13 +1,15 @@
-
 num_guess = ' '
-print("I'm thinking of a number between 1 and 10, what is it?: ")
+print("I'm thinking of a number between 1 and 50, what is it?: ")
 
 
 while num_guess != "no":
-    num_guess = int(input("Would you like to try again??: "))
-    if not num_guess != 33:
-        print("You got it!")
-        break
-    
-
-        
+    try:
+        num_guess = input("Would you like to try again??: ")
+        num_guess = int(num_guess)
+        if  num_guess != 33:
+            input("Try again?")
+        if num_guess == 33:
+            print("You got it!")
+            break
+    except ValueError:
+        print(f'Error: You used {num_guess}, Use digits instead.')
