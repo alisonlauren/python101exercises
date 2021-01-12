54a6e0a823f5fbf6f1b27cd1701842d52fc1578e
Alison Manning <alisonlaurenmanning@gmail.com>
2021-01-12 13:45:28 -0600

    add

--- madlib.py
+++ madlib.py
@@ -1,8 +1,14 @@
 print("Please fill in the blanks below: ")
-user_name = input("What is your name?")
-favorite_subject = input("what is your favorite subject?")
-type_of_vehicle = input("mode of transportation?")
-adjective = input("Adjective?")
+user_name = input("What is your name? ")
+favorite_subject = input("what is your favorite subject? ")
+type_of_vehicle = input("mode of transportation? ")
+adjective = input("Adjective? ")
 print(f""" Hello, {user_name}, word around town is that you love to fill your time 
 learning about {favorite_subject}. I've also heard that you drive a {adjective}
 {type_of_vehicle}. Is this all true? Sounds pretty crazy to me.""")
+
+
+user_input1 = input("Give me a name: ")
+user_input2 = input("Give me a verb: ")
+user_input3 = input("Give me a noun: ")
+print("Hello my name is %s, people say im really good at %s, I learned this at %s" % (user_input1, user_input2, user_input3))
