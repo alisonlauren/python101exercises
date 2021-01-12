7f8d8f73ae35203e3fd8e4609f86bbdb378623fb
Alison Manning <alisonlaurenmanning@gmail.com>
2021-01-12 13:22:33 -0600

    add

--- helloyou2.py
+++ helloyou2.py
@@ -1,6 +1,6 @@
 user_name = input("Hey you, what's your name? ")
 greeting = f"Hello {user_name}, it\'s so great to meet you!"
 count_letters = len(user_name)
-letters_of_name = f"Your name, {user_name} has {count_letters} in it!"
+letters_of_name = f"Your name has {count_letters} in it!"
 print(greeting.upper())
-print(letters_of_name.upper())
+print(letters_of_name)
