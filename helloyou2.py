486328658162e942c8d67dfb183f0debf8042406
Alison Manning <alisonlaurenmanning@gmail.com>
2021-01-12 13:27:19 -0600

    add

--- helloyou2.py
+++ helloyou2.py
@@ -4,3 +4,10 @@ count_letters = len(user_name)
 letters_of_name = f"Your name has {count_letters} in it!"
 print(greeting.upper())
 print(letters_of_name)
+
+
+
+user_input = int(input("Give me a number: "))
+result = user_input * user_input
+greeting2 = f"Thanks! Your number multiplied by itself is {result}"
+print(greeting2)
