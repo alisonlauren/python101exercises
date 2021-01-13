cb982aa1b49c6dfc3163844d4a8265a8ec04d479
Alison Manning <alisonlaurenmanning@gmail.com>
2021-01-13 09:20:14 -0600

    add

---
+++ notes.py
@@ -0,0 +1,11 @@
+# What is an index?
+# reference to where a data is, aka seat numbers
+
+# Basic Loop
+title = "Green Lantern Corp"
+STOP = 10
+counter = 0
+while counter < STOP:
+    print(counter)
+    counter = counter + 1
+
