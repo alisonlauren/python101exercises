cf3390ab869f49a3cf0aab1d617fd4cdb0c35a9e
Alison Manning <alisonlaurenmanning@gmail.com>
2021-01-14 10:27:06 -0600

    more solutions

--- printsquare2.py
+++ printsquare2.py
@@ -9,3 +9,15 @@ for i in range(side):
         print('❄️', end = ' ')
     print()
 
+    #or 
+
+one = 5
+two = 5
+
+counter = 0
+thing = '*' * two
+
+while counter < one:
+    print(thing)
+    counter += 1
+
---
+++ thonnytest.py
@@ -0,0 +1,8 @@
+def no_print():
+    print("Print this")
+    print("Try to print this too")
+    print("Also this, ok?")
+    return
+
+no_print();
+    
